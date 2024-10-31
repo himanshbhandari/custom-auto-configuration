bc8259798b3fc4fb85dccb8ddf00bdfe5d351950
Himanshu Bhandari <himansh8755529677@gmail.com>
2024-10-31 11:04:51 +0530

    intial commit when project started

---
+++ src/main/java/com/example/customAutoConfiguration/CustomAutoConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.customAutoConfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAutoConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAutoConfigurationApplication.class, args);
+	}
+
+}
