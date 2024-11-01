79c236b2bdd235972ee0853af0397b49f08e7b92
Himanshu Bhandari <himansh8755529677@gmail.com>
2024-11-01 08:42:13 +0530

    fourth commit

--- Step_guide_readme.txt
+++ Step_guide_readme.txt
@@ -4,3 +4,4 @@ Second-commit=> remove @Component annotation from top of the SpringBootCourse cl
 
 Third-commit=> create AppConfig class and give them @Configuration annotation and make SpringBootCourseBean inside it.
 
+Fourth-commit=> remove @Configuration replace with @AutoConfiguration annotation on top of the AppConfig class. so now we run it give error so we do some configuration which is spring boot follow so Create META-INF folder -> create spring folder inside it-> create org.springframeworkboot.autoconfigure.AutoConfiguration.imports file -> inside it give complete path or qualified path name of your AppConfig class (Just like Spring boot frame work follow same structure) now run the application so everything working fine
--- src/main/java/com/example/customAutoConfiguration/autoConfig/AppConfig.java
+++ src/main/java/com/example/customAutoConfiguration/autoConfig/AppConfig.java
@@ -1,9 +1,14 @@
 package com.example.customAutoConfiguration.autoConfig;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration  // this annotation tell this class contain beans
+
+/*AutoConfiguration we only use those classes where you do wanted some automatic configuration
+    so if we want our bean which is inside this class . that beans created or configure automatically
+    so we follow same structure which spring boot follow create META-INF->spring->AutoConfiguration file
+ */
+@AutoConfiguration
 public class AppConfig {
     /*
     * instead of create bean inside main class now we do that creating bean n all configuration we do here
